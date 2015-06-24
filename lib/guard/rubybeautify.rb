bb243913590b9f36bd7c7d8eaa7f25747c4d5701
Ernie Brodeur <ebrodeur@ujami.net>
2015-06-23 18:48:52 -0700

    So, guard threads.  moved towards a grace period based delay.

--- lib/guard/rubybeautify.rb
+++ lib/guard/rubybeautify.rb
@@ -2,27 +2,30 @@ require "guard/rubybeautify/version"
 require 'guard/compat/plugin'
 
 module Guard
-  class Rubybeautify < Plugin
-    attr_accessor :count, :style
+	class Rubybeautify < Plugin
+		attr_accessor :count, :style, :grace_period
 
-    def initialize(options = {})
-      super
-      @options = {
-        count: 1,
-        style: :tabs
-      }.merge(options)
-    end
+		def initialize(options = {})
+			super
+      @last_run = 0
+			@options = {
+        grace_period: 5,
+				count: 1,
+				style: :tabs
+			}.merge(options)
+		end
 
-    def run_on_modifications(paths)
-      unless @beautified
-        @beautified = true
-        paths.each do |file|
-		      `ruby-beautify #{file} --overwrite`
-          Compat::UI.info "Ruby beautify executed against #{file}"
-        end
+		def run_on_modifications(paths)
+			if @last_run + options[:grace_period] < Time.now.to_i
+				paths.each do |file|
+					Compat::UI.info "Ruby beautify executed against: #{Guard::Compat::UI.color file, :green}"
+					`ruby-beautify #{file} --overwrite`
+				end
+        # make sure to set a real last run if we did a beautify.
+				@last_run = Time.now.to_i
       else
-        @beautified = false
-      end
-    end
-  end
+				Compat::UI.debug "Ruby beautify skipped on #{paths.join}"
+			end
+		end
+	end
 end
